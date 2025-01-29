3e2af1b9eb318b294605f0abd924c8a1bdae2dcb
Tu nombre <coleccionista84@gmail.com>
2025-01-28 23:04:55 -0600

    Actualización de Angular a la versión 19

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,29 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 
-@NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  exports:[
-    LoginComponent,
-    RegisterComponent,
-    
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ]
-})
+@NgModule({ declarations: [
+        LoginComponent,
+        RegisterComponent
+    ],
+    exports: [
+        LoginComponent,
+        RegisterComponent,
+    ], imports: [CommonModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 
 export class AuthModule { }
